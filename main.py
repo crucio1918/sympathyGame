7d5fcacae1f759c3c51efa173e47ba165fe68681
crucio1918 <48020051+crucio1918@users.noreply.github.com>
2020-02-26 16:03:12 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, VideoSendMessage, StickerSendMessage
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, VideoSendMessage, StickerSendMessage, ConfirmTemplate
 )
 import os
 app = Flask(__name__)
@@ -16,38 +16,59 @@ LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 import random
+import time
 
-def myIndex(l, x):
-    if x in l:
-        return l.index(x)
-    else:
-        return -1
+playerdict = {}   #playerID(user_id)とPlayerインスタンスの紐付け
 
-hands = ["グー","チョキ","パー"]
+playerIDs_SO = []   #ShuffleされたplayerIDs
 
-def hands_to_int(userhand):
-    return myIndex(hands, userhand)
+playerIDs_DO =[]    #参加順のplayerIDs
 
-def select_bothand():
-    return random.randint(0, 2)
+actedNum = 0    #status中で行動を終えたプレイヤーの数
 
-def judge(userhand, bothand):
-    if userhand == -1:
-        message = "グー、チョキ、パーをカタカナで入力してね"
+themes1 = ['a','b','c','d','e']
 
-    else:
-        #0:あいこ　1:botの勝ち　2:userの勝ち
-        status = (userhand - bothand + 3) % 3
-        message = "botは" + hands[bothand] + "でふ\n"
+themes2 = ['f','g','h','i','j']
 
-        if status == 0:
-            message += "あいこだよーん"
-        elif status == 1:
-            message += "あなたのか...負けダヨーン"
-        elif status == 2:
-            message += "あなたの勝ち\nたまたまだね"
+status = 'suspend'
 
-    return message
+class Player():
+  def __init__(self, name,):
+    self.name = name
+    self.answer = ''
+    self.voted = False
+    self.ansVote = 0    #得票数
+
+def question(num):
+        text ='お題は「%s × %s」です。'%(
+          themes1[random.randint(0, len(themes1)-1)], 
+          themes2[random.randint(0, len(themes2)-1)]
+          )
+        TextSendMessage('%sさんの番です'%playerdict{playerIDs_SO[num]}.name)
+        TextSendMessage('抽選中・・・')
+        time.sleep(3.0)
+        TextSendMessage(text)
+        global actedNum
+        actedNum+=1
+
+def createConfirm():
+  confirm_temprate_message = TemplateSendMessage(
+      alt_text='Confirm template',
+      template=ConfirmTemplate(
+          text='参加する？',
+          actions=[
+              PostbackAction(
+                  label='参加',
+                  data='participate'
+              ),
+              PostbackAction(
+                  label='締め切り',
+                  data='close'
+              )
+          ]
+      )
+  )
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -64,11 +85,108 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = judge(hands_to_int(event.message.text), select_bothand())
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=message))
+def handle_text_message(event):
+  global playerdict
+  global playerIDs_SO
+  global playerIDs_DO
+  global actedNum
+  global status
+  text = event.message.text
+  profile = line_bot_api.get_profile(event.source.user_id)
+
+  if text == '強制終了':
+    TextSendMessage('強制終了')
+    status == 'suspend'
+  else:
+    if status == 'suspend':
+      if text =='開始':
+        playerdict = {}
+        playerIDs_SO = []
+        playerIDs_DO = []
+        actedNum = 0
+        TextSendMessage('「開始」はOK')
+        createConfirm()
+        line_bot_api.reply_message(
+          event.reply_token,confirm_temprate_message
+        )
+        status = 'inviting'
+
+    elif status == 'playing':
+      if actedNum == len(playerIDs_SO):
+        TextSendMessage('回答終了。投票に移る')
+        actedNum = 0
+        text =''
+        i = 1
+        for playerID in playerIDs_SO:
+          text += ('answer%d: %s\n'%(i, playerdict{playerID}.answer))
+          i+=1
+        text.rstrip('\n')
+        TextSendMessage(text)
+        status = 'voting'
+      else:
+        if profile.user_id == playerIDs_SO[actedNum]:
+          playerdict{profile.user_id}.answer = text
+          question(actedNum)
+        else:
+          TextSendMessage('%sさんの番です'%playerdict{playerIDs_SO[actedNum]}.name)
+
+    elif status == 'voting':
+      if not playerdict{profile.user_id}.voted:
+        vote_num = int(text)
+        if 1 <= vote_num and vote_num <= len(playerIDs_SO):
+          playerdict{playerIDs_SO[vote_num-1]}.ansVote+=1
+          playerdict{profile.user_id}.voted = True
+          actedNum+=1
+        else:
+          TextSendMessage('answer%s is not exists'%text)
+      else:
+        TextSendMessage('already voted')
+      if actedNum == len(playerIDs_SO):
+        text = '勝者は'
+        winnerIDs = []
+        most = 0
+        for playerID in playerIDs_DO:
+          if playerdict{playerID}.ansVote == most:
+            winnerIDs.append(playerID)
+          elif playerdict{playerID}.ansVote > most:
+            winnerIDs = [playerID]
+            most = playerdict{playerID}.ansVote
+        for winnerID in winnerIDs:
+          text+= 'と%sさん'%playerdict{winnerID}.name
+        text.lstrip('と')
+        text+='です。'
+        TextSendMessage(text)
+        status = 'suspend'
+
+  @handler.add(PostbackEvent)
+  def on_postback(event):
+    global status
+    global playerIDs_SO
+    global playerIDs_DO
+    global playerdict
+
+    if status == 'inviting':
+      profile = line_bot_api.get_profile(event.source.user_id)
+      reply_token = event.reply_token
+      postback_msg = event.postback.data
+      if postback_msg == 'participate':
+        TextSendMessage('「参加」はOK')
+        if not profile.user_id in playerIDs_SO:
+          playerdict{profile.user_id} = Player(profile.display_name)
+          playerIDs_SO.append(profile.user_id)
+      elif postback_msg == 'close':
+        TextSendMessage('「締め切り」はOK')
+        TextSendMessage('募集終了')
+        text = '参加メンバーは以下の通り\n'
+        for playerId in playerIDs_SO:
+          i = 1
+          text += '%d. %s/n'%(i, playerdict{playerId}.display_name)
+        text.rstrip('\n')
+        TextSendMessage(text)
+        playerIDs_DO = playerIDs_SO
+        random.shuffle(playerIDs_SO)
+        question(actedNum)
+        status = 'playing'
 
 if __name__ == "__main__":
 #    app.run()
